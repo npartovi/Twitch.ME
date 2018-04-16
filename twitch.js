4a86d4d85ca6fe3a91d534a607bd5f8881b62e53
npartovi <npartovi@gmail.com>
2018-04-15 21:30:02 -0700

    removed useless code

--- twitch.js
+++ twitch.js
@@ -33,7 +33,6 @@ function loadTopLine(){
 }
 
 
-
 function bubbleChart(){
 
 	$.ajax({
@@ -104,99 +103,9 @@ function bubbleChart(){
 					.attr("cy", function(d){
 						return d.y
 					})
-			}
-
-			 
-
-
-			    
-
-
-
-			
-
+			}	
 		}
 	});
-
-
-
-
-
-
-	// var graphSelection = d3.select(".graph")
-
-	// // selects the "graph" div on the html page and appends a svg container
- //    var svgContainer = graphSelection
- //    			.append("svg")
- //                .attr("width", 960)
- //                .attr("height", 960);
-
- //    // formats numbers by rounding down. ex 6.2 => 6
-	// var format = d3.format(",d");
-
-	// // chooses color scheme for rendering bubbles. more color schemes available.
-	// var color = d3.scaleOrdinal(d3.schemeCategory20c);
-
-	// var node = svgContainer.selectAll("p")
-	//     .data(data)
-	//     .enter()
-	//     .append("p")
-
-	// console.log(gamesArray)
-
-
-	// var pack = d3.pack()
-	//     .size([960, 960])
-	//     .padding(1.5);
-
-	// d3.csv("flare.csv", function(d) {
-	//   d.value = +d.value;
-	//   if (d.value) return d;
-	// }, function(error, classes) {
-	//   if (error) throw error;
-
-	//   var root = d3.hierarchy({children: classes})
-	//       .sum(function(d) { return d.value; })
-	//       .each(function(d) {
-	//         if (id = d.data.id) {
-	//           var id, i = id.lastIndexOf(".");
-	//           d.id = id;
-	//           d.package = id.slice(0, i);
-	//           d.class = id.slice(i + 1);
-	//         }
-	//       });
-
-	 
-	      // .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-	  // node.append("circle")
-	  //     .attr("id", function(d) { return d.id; })
-	  //     .attr("r", function(d) { return d.r; })
-	  //     .style("fill", function(d) { return color(d.package); });
-
-	  // node.append("clipPath")
-	  //     .attr("id", function(d) { return "clip-" + d.id; })
-	  //   .append("use")
-	  //     .attr("xlink:href", function(d) { return "#" + d.id; });
-
-	  // node.append("text")
-	  //     .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-	  //   .selectAll("tspan")
-	  //   .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-	  //   .enter().append("tspan")
-	  //     .attr("x", 0)
-	  //     .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-	  //     .text(function(d) { return d; });
-
-	  // node.append("title")
-	  //     .text(function(d) { return d.id + "\n" + format(d.value); });
-
-
-
-
-
-	// });
-
 }
 
 loadTopLine();
