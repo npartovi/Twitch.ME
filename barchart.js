1957c4e7d2bcadfb72ba400ccf675a8230118227
npartovi <npartovi@gmail.com>
2018-04-19 11:43:05 -0700

    finished mvps, starting on binus features

--- barchart.js
+++ barchart.js
@@ -1,5 +1,10 @@
 const gamesArray = [];
 
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 function loadTableData(){
 
 	$.ajax({
@@ -22,15 +27,16 @@ function loadTableData(){
 
 		gamesList.forEach((d) => {
 			html += '<tr class="hover small"><td class="table-game-name">' + d.game.name + '</td>' +
-					'<td class="table-game-popularity">' + d.game.popularity + '</td>' + 
-					'<td class="table-total-viewers">' + d.viewers + '</td>' +
-					'<td class="table-total-channels">' + d.channels + '</td>' +
+					'<td class="table-game-popularity">' + numberWithCommas(d.game.popularity) + '</td>' + 
+					'<td class="table-total-viewers">' + numberWithCommas(d.viewers) + '</td>' +
+					'<td class="table-total-channels">' + numberWithCommas(d.channels) + '</td>' +
 					'<tr>'
 		});
 
 		$('.table').html(html);
 		document.getElementsByClassName("hover")[0].click()
 		}
+
 	});
 }
 
@@ -38,6 +44,7 @@ function loadTableData(){
 function StreamerBarchart(){
 
 $("body").on("click", ".hover", function(){
+	$(".stream-header").empty()
 	var gamename = $(this).find('td:first-of-type').html();
 	console.log(gamename)
 	let dataset = [];
@@ -55,11 +62,11 @@ $("body").on("click", ".hover", function(){
 			for (var i=0; i < gamesArray[0].length; i++) {
 				if (gamesArray[0][i].game.name === gamename)
 					html2 = '<p class="gametitle">Game: '+gamename+'</p>'
-							+'<h4>Watching: ' + gamesArray[0][i].viewers +'</h4>'
+							+'<h4>Watching: ' + numberWithCommas(gamesArray[0][i].viewers) +'</h4>'
 							+'<h4>Rank: ' + (i+1) + '</h4>'
-							+'<h4>Streaming: '+ gamesArray[0][i].channels +'</h4>';
+							+'<h4>Streaming: '+ numberWithCommas(gamesArray[0][i].channels) +'</h4>';
 			}
-			var pic = data.games[0].box.medium;
+			var pic = data.games[0].box.large;
 			html = '<img class="img-circle" src="'+pic+'">';
 			$(".header").html(html);
 			$(".gamename").html(html2);
@@ -85,8 +92,8 @@ $("body").on("click", ".hover", function(){
 		});
 
 		let margin = {top: 5, right: 20, bottom:30, left: 100};
-		let width = 560 - margin.left - margin.right;
-		let height = 670 - margin.top - margin.bottom;
+		let width = 570 - margin.left - margin.right;
+		let height = 660 - margin.top - margin.bottom;
 
 		let y = d3.scaleBand()
 					.range([height, 0])
@@ -102,6 +109,8 @@ $("body").on("click", ".hover", function(){
     				.attr("height", height + margin.top + margin.bottom)
     				.append("g")
     				.attr("transform", "translate(" + margin.left + "," + margin.top + ")" )
+
+    	$(".stream-header").append(`<h1>${gamename} : Top 20 Streamers</h1>`)
 
 
     	// Scale the range of the data in the domains
@@ -130,7 +139,7 @@ $("body").on("click", ".hover", function(){
 		       div.transition()
 		         .duration(200)
 		         .style("opacity", .9);
-		       div.html("<ul class='tooltip-list'><li><span>Viewers:</span> " + d.viewers + "</li><li><span>Channel:</span> " + d.channel.name + "</li><li><span>Click To View Stream </span></li></ul>")
+		       div.html("<ul class='tooltip-list'><li><span>Viewers:</span> " + numberWithCommas(d.viewers) + "</li><li><span>Channel:</span> " + d.channel.name + "</li><li><span>Click To View Stream </span></li></ul>")
 		         .style("left", (d3.event.pageX) + "px")
 		         .style("top", (d3.event.pageY - 28) + "px");
 		       })
@@ -148,8 +157,8 @@ $("body").on("click", ".hover", function(){
 
 		    	let iframeRender = document.createElement("iframe")
 		    	iframeRender.src = url;
-		    	iframeRender.height = 420;
-		    	iframeRender.width = 640;
+		    	iframeRender.height = 480;
+		    	iframeRender.width = 1080;
 		    	iframeRender.frameborder = 0;
 		    	iframeRender.scrolling = "no";
 		    	iframeRender.allowfullscreen = "yes";							
